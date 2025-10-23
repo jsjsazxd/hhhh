baed8a27fb4848c01add8cc405e4bc0782a44d43
lisi <lisi@126.com>
2025-10-23 19:14:32 +0800

    c2

--- src/main/java/org/zjc/hhh.java
+++ src/main/java/org/zjc/hhh.java
@@ -3,5 +3,7 @@ package org.zjc;
 public class hhh {
     public static void main(String[] args) {
         System.out.println("Hello, hello!");
-    }
+        System.out.println("camionq");
+        }
+
 }
